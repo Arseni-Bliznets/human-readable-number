62578061e9ccec21ba7f506ffa961e94a52c1c48
Arseni-Bliznets <bliznets04@gmail.com>
2023-07-06 16:11:46 +0300

    feat: end tasks

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const hundreds = [,'','one hundred', 'two hundred', 'three hundred', 'four hundr
 if(number < 20) {return arrUnits[number]}
 if(Number(number.toString().slice(1)) === 0 && number < 100) {return arrDozens[Number(number.toString().slice(-2,-1))]}//20,30,40
 if(Number(number.toString().slice(1)) !== 0 && number < 100) {return arrDozens[Number(number.toString().slice(-2,-1))] + ' ' + arrUnits[Number(number.toString().slice(-1))]}//21,32
-if(Number(number.toString().slice(1)) === 00 && number >= 100){return hundreds[Number(number.toString().slice(0,1)) + 1]}//100,300
-if(Number(number.toString().slice(1)) > 00 && number >= 100 && Number(number.toString().slice(1)) < 20)
+if(Number(number.toString().slice(1)) === 0o0 && number >= 100){return hundreds[Number(number.toString().slice(0,1)) + 1]}//100,300
+if(Number(number.toString().slice(1)) > 0o0 && number >= 100 && Number(number.toString().slice(1)) < 20)
 {return hundreds[Number(number.toString().slice(0,1)) + 1] + ' ' + arrUnits[Number(number.toString().slice(1))]}//101 - 119, 201 - 219
 if(Number(number.toString().slice(1)) > 19 && Number(number.toString().slice(2,3)) === 0 && number > 100)
 {return hundreds[Number(number.toString().slice(0,1)) + 1] + ' ' + arrDozens[Number(number.toString().slice(1,2))]}//150,250
